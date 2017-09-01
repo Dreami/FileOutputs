9e9d5b935dcd295d7d8d662341a605c3c64d07ca
dreamiland <maple_jista@hotmail.com>
2017-08-31 19:28:53 -0500

    Add project files.

---
+++ FileOutputs/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOutputs
+{
+    public class FileOutputs
+    {
+        public static void output_print(String output_path, String output)
+        {
+            try
+            {
+                if (!File.Exists(output_path))
+                {
+                    using (var stream = File.Create(output_path)) ;
+                    TextWriter tw = new StreamWriter(output_path, false);
+                    tw.WriteLine(output);
+                    tw.Close();
+                }
+                else if (File.Exists(output_path))
+                {
+                    using (var tw = new StreamWriter(output_path, true))
+                    {
+                        tw.WriteLine(output);
+                        tw.Close();
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException directoryExc)
+            {
+                Console.WriteLine(directoryExc.StackTrace);
+            }
+            catch (IOException ioExc)
+            {
+                Console.WriteLine(ioExc.StackTrace);
+            }
+            catch (UnauthorizedAccessException unauthorizedExc)
+            {
+                Console.WriteLine(unauthorizedExc.StackTrace);
+            }
+        }
+    }
+}
