d353f80c25273b55cb520f54e4aeca347f41549a
dreamiland <maple_jista@hotmail.com>
2017-10-22 04:21:16 -0500

    Not sure what I just updated.

--- FileOutputs/Class1.cs
+++ FileOutputs/Class1.cs
@@ -9,6 +9,9 @@ namespace FileOutputs
 {
     public static class Outputs
     {
+        private static string allfiles = @"C:\Users\maple\Documents\9° Semester\CS13309_Archivos_HTML\Files";
+        private static string allOutputFiles = @"C:\Users\maple\Documents\9° Semester\CS13309_Archivos_HTML";
+
         public static void output_print(String output_path, String output)
         {
             try
@@ -41,6 +44,16 @@ namespace FileOutputs
             {
                 Console.WriteLine(unauthorizedExc.StackTrace);
             }
+        }
+
+        public static String getAllFiles()
+        {
+            return allfiles;
+        }
+
+        public static String getOutputFiles()
+        {
+            return allOutputFiles;
         }
     }
 }
